e30423b9dfa9d23c90dda6464ec5dcadfeb5e54d
Aleksey Motovilov <lehaqs@gmail.com>
2017-06-06 17:41:54 +0300

    Fix cs error.

--- src/scripts/app/main.module.ts
+++ src/scripts/app/main.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, PreloadAllModules } from '@angular/router';
 
 import { MainComponent } from './main.component';
 import { HomeComponent } from './components/home/home.component';
-import { MathService } from './components/home/math.service'
+import { MathService } from './components/home/math.service';
 import { Routes } from './main.routing';
 
 @NgModule({
