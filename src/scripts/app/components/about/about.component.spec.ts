fd8399773f9645c1e702b7e8eeece421885a25db
Aleksey Motovilov <lehaqs@gmail.com>
2017-06-08 14:44:04 +0300

    Added few static pages.

---
+++ src/scripts/app/components/about/about.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { AboutComponent } from './about.component';
+
+describe('AboutComponent', () => {
+	let component: AboutComponent;
+	let fixture: ComponentFixture<AboutComponent>;
+
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			declarations: [ AboutComponent ],
+			imports: [ RouterTestingModule ],
+		});
+
+		fixture = TestBed.createComponent(AboutComponent);
+		component = fixture.componentInstance;
+	});
+
+	it('About initialized', () => {
+		expect(fixture).toBeDefined();
+		expect(component).toBeDefined();
+	});
+});
---
+++ src/scripts/app/components/contacts/contacts.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { ContactsComponent } from './contacts.component';
+
+describe('ContactsComponent', () => {
+	let component: ContactsComponent;
+	let fixture: ComponentFixture<ContactsComponent>;
+
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			declarations: [ ContactsComponent ],
+			imports: [ RouterTestingModule ],
+		});
+
+		fixture = TestBed.createComponent(ContactsComponent);
+		component = fixture.componentInstance;
+	});
+
+	it('Contacts initialized', () => {
+		expect(fixture).toBeDefined();
+		expect(component).toBeDefined();
+	});
+});
